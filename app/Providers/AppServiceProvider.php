6f5cdff957570e53f5e00b1549071d4d85f5afb7
Yon <bollainbermejoyon@gmail.com>
2024-01-09 18:14:47 +0100

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,9 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Validator::extend('file_ext', function ($attribute, $value, $parameters, $validator) {
             $ext = strtolower($value->getClientOriginalExtension());
             return in_array($ext, ['png', 'jpg', 'pdf']);
         });
+        
     }
 }
